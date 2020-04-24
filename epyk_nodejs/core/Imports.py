bc916b3e0adedf4f90eb1a856a711bff2e3e5dd8
marlyk <leemark0071@gmail.com>
2020-04-24 21:41:47 +0100

    Start imports

---
+++ epyk_nodejs/core/Imports.py
@@ -0,0 +1,6 @@
+
+
+PACKAGES_IMPORTS = {
+  'jquery': {"alias": 'jquery', 'name': '$ = jQuery'}, # root the two names
+  'jquery-ui': {"alias": 'jquery-ui', 'name': 'jQueryUI', 'req': ['jquery']}
+}
--- epyk_nodejs/core/PyOutNode.py
+++ epyk_nodejs/core/PyOutNode.py
@@ -14,7 +14,7 @@ class PyOuts(PyOuts.PyOuts):
       sub_path = os.path.join(path, folder)
       if not os.path.exists(sub_path):
         os.makedirs(sub_path, exist_ok=True)
-    htmlParts, cssParts = ["<link rel='stylesheet' href='/stylesheets/%s.css' />" % self._options['module']], dict(self._report.body.style.get_classes_css())
+    htmlParts, cssParts = ["<head><link rel='stylesheet' href='/stylesheets/%s.css' /><style>%s</style></head>" % (self._options['module'], "\n".join(self._report._cssText))], dict(self._report.body.style.get_classes_css())
     for objId in self._report.content:
       if self._report.htmlItems[objId].inReport:
         htmlParts.append(self._report.htmlItems[objId].html())
@@ -30,12 +30,12 @@ class PyOuts(PyOuts.PyOuts):
     with open(os.path.join(path, "modules", "%s.js" % self._options['module']), "w") as f:
       for c, d in self._report._props.get('js', {}).get("constructors", {}).items():
         f.write("exports.%s = function(htmlObj%s" % (c,  d.split("function %s(htmlObj" % c)[-1]))
-        #f.write("%s %s\n" % (c, d))
 
     # Then write the app itself
     with open(os.path.join(path, "pages", "%s.html" % self._options['module']), "w") as f:
-      f.write("\n".join(htmlParts))
+      f.write("<body>%s</body>"% "\n".join(htmlParts))
 
+    print(self._report.jsImports)
     #body = str(self._report.body.set_content(self._report, "\n".join(htmlParts)))
     #esults = self._to_html_obj(htmlParts, cssParts)
     #print(results)
